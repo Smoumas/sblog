3264e90f4ac075d8f88632a7ef0c78d18fa138fd
Smoumas <aa497229845@qq.com>
2019-03-30 22:33:33 +0800

    first commit

---
+++ src/main/java/my/blog/controller/HelloController.java
@@ -0,0 +1,16 @@
+package my.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/myblog")
+    public String hello(){
+        return "Welcome to my blog.";
+    }
+}
