9295fa9e9af8f5245853c8171c522493911af228
Smoumas <aa497229845@qq.com>
2019-04-10 22:41:17 +0800

    完成展示博客列表功能，并成功访问

--- src/main/java/my/blog/controller/BlogController.java
+++ src/main/java/my/blog/controller/BlogController.java
@@ -1,5 +1,7 @@
 package my.blog.controller;
 
+import my.blog.config.ServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.ClassUtils;
@@ -15,6 +17,9 @@ import java.util.Map;
 @RequestMapping("/blogs")
 public class BlogController {
 
+    @Autowired
+    private ServerConfig serverConfig;
+
     @RequestMapping("/{ID}")
     public String listBlogs(@PathVariable String ID,Model model){
         String resourcePath = ClassUtils.getDefaultClassLoader().getResource("").getPath();
@@ -24,7 +29,8 @@ public class BlogController {
         for(File blog:blogs){
             String fileName = blog.getName();
             String []results = resourcePath.split("classes");
-            String filePath = results[1]+"/blogs/"+ID+"/"+fileName;
+            System.out.println("Result1:"+results[0]);
+            String filePath = serverConfig.getURL()+results[1]+"/blogs/"+ID+"/"+fileName;
             System.out.println(filePath);
             fileMap.put(fileName,filePath);
         }
